c45f6b3493eb88a5811e6c36fb85004b0f99b593
Roy <royfansyahae@gmail.com>
2023-10-17 11:03:05 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String apiUrl = "http://localhost:8012/api/list.php";
+  final String apiUrl = "http://localhost:8012/api/list.php"; // lokal
 
   Future<List<dynamic>> fetchData() async {
     final response = await http.get(Uri.parse(apiUrl));
 
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
+      final data = json.decode(response.body)['data'];
       return data;
     } else {
       throw Exception('Failed to load data');
@@ -32,40 +32,37 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Belajar API'),
+        title: Text('BELAJAR RESTFUL API'),
       ),
-      body: Container(
-        child: FutureBuilder(
-          future: fetchData(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text('Error: ${snapshot.error}'),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchData(),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              padding: EdgeInsets.all(10),
+              itemCount: snapshot.data?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: Image.network(
+                            'http://localhost:8012/api/img/' +
+                                snapshot.data![index]['avatar']) // lokal
+                        .image,
+                  ),
+                  title: Text(snapshot.data![index]['first_name'] +
+                      " " +
+                      snapshot.data![index]['last_name']),
+                  subtitle: Text(snapshot.data![index]['produk']),
                 );
-              } else {
-                List<dynamic> data = snapshot.data;
-                return ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading: Image.network('http://localhost:8012/api/img/' +
-                          data[index]['avatar']),
-                      title: Text(data[index]['first_name'] +
-                          ' ' +
-                          data[index]['last_name']),
-                      subtitle: Text(data[index]['produk']),
-                    );
-                  },
-                );
-              }
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
+              },
+            );
+          }
+        },
       ),
     );
   }
